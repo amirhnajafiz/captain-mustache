50b414e647b8e70c95e67d7decf28548d5b43952
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-09 19:16:54 +0330

    init: commands functions

--- internal/commands.go
+++ internal/commands.go
@@ -1,1 +1,21 @@
 package internal
+
+// UpCommand starts docker containers.
+func UpCommand() {
+
+}
+
+// DownCommand stops docker containers.
+func DownCommand() {
+
+}
+
+// StatusCommand returns a status of containers.
+func StatusCommand() {
+
+}
+
+// BuildCommand generates docker compose manifest.
+func BuildCommand() {
+
+}

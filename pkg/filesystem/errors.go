598dc50ec2d6cbe31557cbdb0e6d794ed0057e62
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-10 11:39:04 +0330

    add: filesystem write method

--- pkg/filesystem/errors.go
+++ pkg/filesystem/errors.go
@@ -4,4 +4,6 @@ import "errors"
 
 var (
 	ErrFileNotFound = errors.New("file not found")
+	ErrCreateFile   = errors.New("failed to create file")
+	ErrSystemFile   = errors.New("failed to write into file")
 )
--- pkg/filesystem/filesystem.go
+++ pkg/filesystem/filesystem.go
@@ -14,3 +14,18 @@ func ReadFile(path string) (io.Reader, error) {
 
 	return bufio.NewReader(f), nil
 }
+
+func WriteFile(reader io.Reader, path string) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return ErrCreateFile
+	}
+
+	w := bufio.NewWriter(f)
+
+	if _, err := w.ReadFrom(reader); err != nil {
+		return ErrSystemFile
+	}
+
+	return nil
+}

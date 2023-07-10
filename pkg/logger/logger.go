3cfac251b13aec4f358f08cd8b3d51c525e59dbe
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-10 11:27:53 +0330

    add: error methods

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -6,5 +6,13 @@ import (
 )
 
 func Error(error error) {
-	log.Printf("[ERROR] %s | error: %v", time.Now().Format(time.DateTime), error)
+	log.Printf("[ERROR] %s\n\terror: %v\n", time.Now().Format(time.DateTime), error)
+}
+
+func Info(message string) {
+	log.Printf("[INFO] %s\n\tmessage: %s\n", time.Now().Format(time.DateTime), message)
+}
+
+func Debug(data string) {
+	log.Printf("[DEBUG] %s\n\tdata: %s\n", time.Now().Format(time.DateTime), data)
 }

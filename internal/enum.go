ef1da26905ace0d29758d0a1caf28956a8c74806
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-31 11:44:27 +0330

    update: enums

--- internal/enum.go
+++ internal/enum.go
@@ -1,5 +1,7 @@
 package internal
 
+import "github.com/AlecAivazis/survey/v2"
+
 type (
 	CommandType int
 	StubType    int
@@ -14,4 +16,38 @@ const (
 	DatabaseStub StubType = iota + 1
 	VolumeStub
 	NetworkStub
+)
+
+var (
+	dockerfileQuestions = []*survey.Question{
+		{
+			Name: "go-version",
+			Prompt: &survey.Select{
+				Message: "Choose your Go version:",
+				Options: []string{"1.16", "1.17", "1.18", "1.19", "1.20"},
+				Default: "1.20",
+			},
+		},
+		{
+			Name: "architecture",
+			Prompt: &survey.Select{
+				Message: "Choose your Go architecture:",
+				Options: []string{"arm64", "amd64"},
+				Default: "arm64",
+			},
+		},
+		{
+			Name: "operating-system",
+			Prompt: &survey.Select{
+				Message: "Choose your Go operating system:",
+				Options: []string{"linux", "darwin", "windows"},
+				Default: "linux",
+			},
+		},
+	}
+
+	dbQuestion = &survey.Select{
+		Message: "Choose a database:",
+		Options: []string{"redis", "mysql", "mongodb", "postgresql"},
+	}
 )
